49f0ff6aa5c8ee313ee08301286a55edc3ca03fd
rahman.muraman <rahman.muraman@gmail.com>
2024-06-27 13:42:35 +0700

    ref: config

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,16 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const db = new Sequelize("db_sl_refresh_token", "postgres", "postgres123", {
-  host: "localhost",
-  dialect: "postgres",
-});
+dotenv.config();
+
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
 
 export default db;
