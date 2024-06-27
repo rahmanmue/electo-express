e3ab9087f909ef19b07a085c4728e849c72ddd61
rahman.muraman <rahman.muraman@gmail.com>
2024-06-27 13:46:06 +0700

    refactor

--- index.js
+++ index.js
@@ -4,25 +4,13 @@ import router from "./routes/index.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import Users from "./models/UserModel.js";
+import User from "./models/UserModel.js";
 import Dapil from "./models/DapilModel.js";
 import SuaraParpol from "./models/SuaraParpolModel.js";
 
 dotenv.config();
 const app = express();
-
-try {
-  await db.authenticate();
-  console.log("Database Connected....");
-
-  //   menggenerate otomatis db
-  //   karena sudah dibuat komenkan lagi
-  // await Users.sync();
-  // await Dapil.sync();
-  // await SuaraParpol.sync();
-} catch (error) {
-  console.error(error);
-}
+const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
@@ -34,4 +22,28 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
-app.listen(5000, () => console.log("Server running at port 5000"));
+const checkAndCreateTable = async (model) => {
+  const tableName = model.getTableName();
+  const tableExists = await db
+    .getQueryInterface()
+    .showAllSchemas({ where: { table_name: tableName } });
+  if (!tableExists.length) {
+    await model.sync({ alter: true });
+    console.log(`Table ${tableName} created.`);
+  } else {
+    console.log(`Table ${tableName} already exists.`);
+  }
+};
+
+(async () => {
+  try {
+    await db.authenticate();
+    await checkAndCreateTable(User);
+    await checkAndCreateTable(Dapil);
+    await checkAndCreateTable(SuaraParpol);
+    console.log("Database Connected...");
+    app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+})();
