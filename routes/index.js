9b38813b3ae057a6557b49f87d5bfd8d14efd79e
rahman.muraman <rahman.muraman@gmail.com>
2024-06-27 13:44:44 +0700

    ref: routes

--- routes/index.js
+++ routes/index.js
@@ -4,33 +4,35 @@ import {
   getUserById,
   updateUserById,
   deleteUserById,
-  Register,
-  Login,
-  Logout,
-} from "../controllers/Users.js";
+} from "../controllers/UserController.js";
+import {
+  register,
+  login,
+  logout,
+  refreshToken,
+} from "../controllers/authController.js";
 import {
   insertDapil,
   getAllDapil,
   getDapilById,
   updateDapilById,
   deleteDapilById,
-} from "../controllers/Dapil.js";
+} from "../controllers/DapilController.js";
 import {
   insertBulkSuaraParpol,
   getSuaraParpolByDapilId,
   getSuaraParpolById,
   updateSuaraParpolById,
   deleteSuaraParpolById,
-} from "../controllers/SuaraParpol.js";
-import { verifyToken } from "../middleware/verifyToken.js";
-import { refreshToken } from "../controllers/RefreshToken.js";
+} from "../controllers/SuaraParpolController.js";
+import { verifyToken } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/register", Register);
-router.post("/login", Login);
-router.get("/token", refreshToken);
-router.delete("/logout", Logout);
+router.post("/register", register);
+router.post("/login", login);
+router.get("/refresh-token", refreshToken);
+router.delete("/logout", logout);
 
 router.get("/users", getUsers);
 router.get("/users/:id", verifyToken, getUserById);
@@ -39,14 +41,14 @@ router.delete("/users/:id", verifyToken, deleteUserById);
 
 router.get("/dapil/:user_id", getAllDapil);
 router.get("/detail-dapil/:id", getDapilById);
-router.post("/dapil",verifyToken, insertDapil);
-router.put("/dapil/:id",verifyToken, updateDapilById);
-router.delete("/dapil/:id",verifyToken, deleteDapilById);
+router.post("/dapil", verifyToken, insertDapil);
+router.put("/dapil/:id", verifyToken, updateDapilById);
+router.delete("/dapil/:id", verifyToken, deleteDapilById);
 
 router.get("/parpol/dapil/:dapil_id", getSuaraParpolByDapilId);
 router.get("/parpol/suara/:id", getSuaraParpolById);
-router.put("/parpol/suara/:id",verifyToken, updateSuaraParpolById);
-router.delete("/parpol/suara/:id",verifyToken, deleteSuaraParpolById);
-router.post("/parpol",verifyToken, insertBulkSuaraParpol);
+router.put("/parpol/suara/:id", verifyToken, updateSuaraParpolById);
+router.delete("/parpol/suara/:id", verifyToken, deleteSuaraParpolById);
+router.post("/parpol", verifyToken, insertBulkSuaraParpol);
 
 export default router;
