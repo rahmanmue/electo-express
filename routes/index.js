d6bf07b260abed4215eeff2dba36cb371e0e60fa
rahman.muraman <rahman.muraman@gmail.com>
2024-06-28 08:35:55 +0700

    feat: update routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {
-  getUsers,
+  getAllUsers,
   getUserById,
   updateUserById,
   deleteUserById,
@@ -12,18 +12,19 @@ import {
   refreshToken,
 } from "../controllers/authController.js";
 import {
-  insertDapil,
+  createDapil,
   getAllDapil,
   getDapilById,
   updateDapilById,
   deleteDapilById,
 } from "../controllers/DapilController.js";
 import {
-  insertBulkSuaraParpol,
+  createBulkSuaraParpol,
   getSuaraParpolByDapilId,
   getSuaraParpolById,
   updateSuaraParpolById,
   deleteSuaraParpolById,
+  getCalculationSuaraParpolByDapilId,
 } from "../controllers/SuaraParpolController.js";
 import { verifyToken } from "../middlewares/authMiddleware.js";
 
@@ -34,14 +35,14 @@ router.post("/login", login);
 router.get("/refresh-token", refreshToken);
 router.delete("/logout", logout);
 
-router.get("/users", getUsers);
+router.get("/users", getAllUsers);
 router.get("/users/:id", verifyToken, getUserById);
 router.patch("/users/:id", verifyToken, updateUserById);
 router.delete("/users/:id", verifyToken, deleteUserById);
 
 router.get("/dapil/:user_id", getAllDapil);
 router.get("/detail-dapil/:id", getDapilById);
-router.post("/dapil", verifyToken, insertDapil);
+router.post("/dapil", verifyToken, createDapil);
 router.put("/dapil/:id", verifyToken, updateDapilById);
 router.delete("/dapil/:id", verifyToken, deleteDapilById);
 
@@ -49,6 +50,8 @@ router.get("/parpol/dapil/:dapil_id", getSuaraParpolByDapilId);
 router.get("/parpol/suara/:id", getSuaraParpolById);
 router.put("/parpol/suara/:id", verifyToken, updateSuaraParpolById);
 router.delete("/parpol/suara/:id", verifyToken, deleteSuaraParpolById);
-router.post("/parpol", verifyToken, insertBulkSuaraParpol);
+router.post("/parpol", verifyToken, createBulkSuaraParpol);
+
+router.post("/hitung/:kursi", getCalculationSuaraParpolByDapilId);
 
 export default router;
