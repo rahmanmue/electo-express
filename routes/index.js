1383dfa551ec02de51c7f7aa70af826e518934e0
rahman.muraman <rahman.muraman@gmail.com>
2024-10-01 21:46:23 +0700

    feat: update api endpoint

--- routes/index.js
+++ routes/index.js
@@ -62,74 +62,82 @@ router.post("/auth/login", login);
 router.get("/auth/refresh-token", refreshToken);
 router.delete("/auth/logout", logout);
 
+// router.get("/users", getAllUsers);
+// router.get("/users/search", searchUser);
+// router.get("/user", getUserByToken);
+// router.patch("/users", updateUserById);
+// router.delete("/users/:id", deleteUserById);
+
 router.get("/users", getAllUsers);
 router.get("/users/search", searchUser);
-router.get("/user", getUserByToken);
-router.patch("/users", updateUserById);
-router.delete("/users/:id", deleteUserById);
-
-// router.get("/users", getAllUsers);
-// router.get("/user", verifyToken, getUserByToken);
-// router.patch("/users", verifyToken, updateUserById);
-// router.delete("/users/:id", verifyToken, verifyAdmin, deleteUserById);
+router.get("/user", verifyToken, getUserByToken);
+router.patch("/users", verifyToken, updateUserById);
+router.delete("/users/:id", verifyToken, verifyAdmin, deleteUserById);
 
 // router.get("/dapil", getAllDapil);
-// router.get("/dapil/search", verifyToken, searchDapil);
-// router.get("/dapil/:id", verifyToken, getDapilById);
-// router.post("/dapil", verifyToken, createDapil);
-// router.put("/dapil", verifyToken, updateDapilById);
-// router.delete("/dapil/:id", verifyToken, deleteDapilById);
+// router.get("/dapil/search", searchDapil);
+// router.get("/dapil/:id", getDapilById);
+// router.post("/dapil", createDapil);
+// router.put("/dapil", updateDapilById);
+// router.delete("/dapil/:id", deleteDapilById);
 
 router.get("/dapil", getAllDapil);
-router.get("/dapil/search", searchDapil);
-router.get("/dapil/:id", getDapilById);
-router.post("/dapil", createDapil);
-router.put("/dapil", updateDapilById);
-router.delete("/dapil/:id", deleteDapilById);
+router.get("/dapil/search", verifyToken, searchDapil);
+router.get("/dapil/:id", verifyToken, getDapilById);
+router.post("/dapil", verifyToken, createDapil);
+router.put("/dapil", verifyToken, updateDapilById);
+router.delete("/dapil/:id", verifyToken, deleteDapilById);
 
+// router.get("/parpol", getAllParpol);
+// router.get("/parpol/search", searchParpol);
+// router.get("/download-parpol", downloadParpol);
+// router.get("/parpol/:id", getParpolById);
+// router.post("/parpol", createParpol);
+// router.put("/parpol", updateParpolById);
+// router.delete("/parpol/:id", deleteParpolById);
+
+router.get("/download-parpol", downloadParpol);
 router.get("/parpol", getAllParpol);
 router.get("/parpol/search", searchParpol);
-router.get("/download-parpol", downloadParpol);
-router.get("/parpol/:id", getParpolById);
-router.post("/parpol", createParpol);
-router.put("/parpol", updateParpolById);
-router.delete("/parpol/:id", deleteParpolById);
+router.get("/parpol/:id", verifyToken, verifyAdmin, getParpolById);
+router.post("/parpol", verifyToken, verifyAdmin, createParpol);
+router.put("/parpol", verifyToken, verifyAdmin, updateParpolById);
+router.delete("/parpol/:id", verifyToken, verifyAdmin, deleteParpolById);
+
+// router.get("/parpol/dapil/:dapil_id", getSuaraParpolByDapilId);
+// router.get("/parpol/vote/:id", getSuaraParpolById);
+// router.put("/parpol/vote", updateSuaraParpolById);
+// router.delete("/parpol/vote/:id", deleteSuaraParpolById);
+// router.post("/parpol/vote", createBulkSuaraParpol);
+
+router.get("/parpol/dapil/:dapil_id", verifyToken, getSuaraParpolByDapilId);
+router.get("/parpol/vote/:id", verifyToken, getSuaraParpolById);
+router.put("/parpol/vote", verifyToken, updateSuaraParpolById);
+router.delete("/parpol/vote/:id", verifyToken, deleteSuaraParpolById);
+router.post("/parpol/vote", verifyToken, createBulkSuaraParpol);
 
 router.get("/all-dapil", getAllDapilForHome);
 router.get("/calculation/:dapil_id", getCalculationSuaraParpol);
 
-// router.get("/parpol/dapil/:dapil_id", verifyToken, getSuaraParpolByDapilId);
-// router.get("/parpol/vote/:id", verifyToken, getSuaraParpolById);
-// router.put("/parpol/vote", verifyToken, updateSuaraParpolById);
-// router.delete("/parpol/vote/:id", verifyToken, deleteSuaraParpolById);
-// router.post("/parpol/vote", verifyToken, createBulkSuaraParpol);
+// router.put("/update-profile", upload.single("avatar"), updateProfileUser);
 
-router.get("/parpol/dapil/:dapil_id", getSuaraParpolByDapilId);
-router.get("/parpol/vote/:id", getSuaraParpolById);
-router.put("/parpol/vote", updateSuaraParpolById);
-router.delete("/parpol/vote/:id", deleteSuaraParpolById);
-router.post("/parpol/vote", createBulkSuaraParpol);
-
-router.put("/update-profile", upload.single("avatar"), updateProfileUser);
-
-// router.put(
-//   "/update-profile",
-//   upload.single("avatar"),
-//   verifyToken,
-//   updateProfileUser
-// );
+router.put(
+  "/update-profile",
+  upload.single("avatar"),
+  verifyToken,
+  updateProfileUser
+);
 
 router.post("/import-excel", uploadExcel.single("document"), importFromExcel);
 
+// router.get("/profile", getProfileUser);
+router.get("/profile", verifyToken, getProfileUser);
 router.get("/avatar/:avatar", getImageAvatar);
-// router.get("/profile", verifyToken, getProfileUser);
-router.get("/profile", getProfileUser);
 
 router.post("/forget-password", forgetPasswordUser);
 router.post("/reset-password/:token", resetPasswordUser);
 
 router.get("/google", loginGoogle);
-
 router.get("/google/callback", loginGoogleCallback);
 
 export default router;
