e26db4a421b55c719ba4d438646e395477968f6d
rahman.muraman <rahman.muraman@gmail.com>
2024-10-04 16:30:26 +0700

    feat: deploy on vercel

---
+++ api/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import db from "../config/database.js";
+import router from "../routes/index.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import User from "../models/UserModel.js";
+import Dapil from "../models/DapilModel.js";
+import SuaraParpol from "../models/SuaraParpolModel.js";
+import Profile from "../models/ProfileModel.js";
+import PasswordResetToken from "../models/ResetPasswordToken.js";
+import "../models/Association.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "*",
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("This is where it all begins 🚀🚀");
+});
+
+app.use(router);
+
+(async () => {
+  try {
+    await db.sync();
+    console.log("Database connected");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  } catch (err) {
+    console.log(err);
+  }
+})();
+
+export default app;
--- config/database.js
+++ config/database.js
@@ -3,14 +3,32 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-  }
-);
+// Local
+// const db = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: process.env.DB_DIALECT,
+//   }
+// );
+
+// Clever Cloud
+const db = new Sequelize(process.env.POSTGRESQL_URI, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true, // Menentukan apakah SSL diperlukan atau tidak
+      rejectUnauthorized: false, // Biasanya diatur ke false di beberapa host
+    },
+  },
+  pool: {
+    max: 10, // Maksimal 10 koneksi bersamaan
+    min: 2, // Minimal 2 koneksi
+    acquire: 30000, // Timeout 30 detik untuk mendapatkan koneksi
+    idle: 10000, // Timeout 10 detik untuk koneksi idle
+  },
+});
 
 export default db;
