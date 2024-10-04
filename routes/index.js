ceaf6397f705a277817fcc64cabb4ac33cc0bff5
rahman.muraman <rahman.muraman@gmail.com>
2024-10-05 06:11:18 +0700

    feat: update routes

--- routes/index.js
+++ routes/index.js
@@ -68,9 +68,9 @@ router.delete("/auth/logout", logout);
 // router.patch("/users", updateUserById);
 // router.delete("/users/:id", deleteUserById);
 
-router.get("/users", getAllUsers);
-router.get("/users/search", searchUser);
-router.get("/user", verifyToken, getUserByToken);
+router.get("/users", verifyToken, verifyAdmin, getAllUsers);
+router.get("/users/search", verifyToken, verifyAdmin, searchUser);
+router.get("/user", verifyToken, verifyAdmin, getUserByToken);
 router.patch("/users", verifyToken, updateUserById);
 router.delete("/users/:id", verifyToken, verifyAdmin, deleteUserById);
 
