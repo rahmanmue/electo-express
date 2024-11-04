ad761b942fe4d00240485053e91b90f77f3ff095
rahman.muraman <rahman.muraman@gmail.com>
2024-11-04 17:39:50 +0700

    feat: update key

--- config/firebase-admin.js
+++ config/firebase-admin.js
@@ -3,22 +3,10 @@ dotenv.config();
 
 import admin from "firebase-admin";
 
-const key = {
-  type: process.env.TYPE,
-  project_id: process.env.PROJECT_ID,
-  private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email: process.env.CLIENT_EMAIL,
-  client_id: process.env.CLIENT_ID,
-  auth_uri: process.env.AUTH_URI,
-  token_uri: process.env.TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.CLIENT_CERT_URL,
-  universe_domain: process.env.UNIVERSE_DOMAIN,
-};
+const key = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
-  credential: admin.credential.cert(JSON.stringify(key)),
+  credential: admin.credential.cert(key),
   storageBucket: "final-project-fullstack.appspot.com",
 });
 
