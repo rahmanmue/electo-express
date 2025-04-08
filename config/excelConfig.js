f06084bb4ecfeb0ae224569aa4767999a17addb2
rahmanmue <rahman.muraman@gmail.com>
2025-04-08 18:40:50 +0700

    refactor: move to src folder

--- config/excelConfig.js
+++
@@ -1,4 +0,0 @@
-export const expectedColumns = [
-  { name: "nama_parpol", type: "string" },
-  { name: "total_suara_sah", type: "number" },
-];
--- middlewares/uploadMemoryMiddleware.js
+++
@@ -1,9 +0,0 @@
-import multer from "multer";
-import { fileFilter } from "./uploadExcelMiddleware.js";
-
-export const uploadAvatarMemory = multer({ storage: multer.memoryStorage() });
-export const uploadExcelMemory = multer({
-  storage: multer.memoryStorage(),
-  fileFilter: fileFilter,
-  limits: { fileSize: 1024 * 1024 * 5 },
-});
